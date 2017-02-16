6399854f05d8e3688f88ec6cb7d37815e579a7f0
Marius Strandhaug <mariuss@mesan.no>
2017-02-16 16:57:38 +0100

    La til noen oppgaver

--- Refactoring/with-clause/before.sql
+++ Refactoring/with-clause/before.sql
@@ -3,7 +3,6 @@ FROM Medlem m
   INNER JOIN Ordre o
     ON o.medlemid = m.id
 WHERE m.aktiv = 1
-      AND 5 <= (
-  SELECT SUM(ol.antall)
-  FROM Ordrelinje ol
-  WHERE ol.ordreid = o.id);
+      AND 5 <= (SELECT SUM(ol.antall)
+                FROM Ordrelinje ol
+                WHERE ol.ordreid = o.id);
