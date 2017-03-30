d3c5b35fe4c4fe18dc4f86bed82f6fac9089e975
Marius Strandhaug <mariuss@mesan.no>
2017-03-30 17:13:56 +0200

    Oppgave 5

--- Database/schema.sql
+++ Database/schema.sql
@@ -3,26 +3,60 @@ DROP TABLE IF EXISTS Ordre;
 DROP TABLE IF EXISTS Medlem;
 
 CREATE TABLE Medlem (
-  id INTEGER PRIMARY KEY,
-  navn VARCHAR,
+  id    INTEGER PRIMARY KEY,
+  navn  VARCHAR,
   aktiv INTEGER DEFAULT 1
 );
 
 CREATE TABLE Ordre (
-  id INTEGER PRIMARY KEY,
-  medlemId INTEGER REFERENCES Medlem(id) ON DELETE CASCADE
+  id        INTEGER PRIMARY KEY,
+  medlemId  INTEGER REFERENCES Medlem (id) ON DELETE CASCADE,
+  ordreDato TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Vare (
+  varenummer INTEGER,
+  versjon    INTEGER,
+  varenavn   VARCHAR NOT NULL,
+  pris       INTEGER NOT NULL,
+  PRIMARY KEY (varenummer, versjon)
 );
 
 CREATE TABLE OrdreLinje (
-  id INTEGER PRIMARY KEY,
-  ordreId INTEGER REFERENCES Ordre(id) ON DELETE CASCADE,
-  antall INTEGER DEFAULT 1,
-  enhetsPris INTEGER
+  id          INTEGER PRIMARY KEY,
+  ordreId     INTEGER REFERENCES Ordre (id) ON DELETE CASCADE,
+  varenummer  INTEGER,
+  vareversjon INTEGER,
+  antall      INTEGER DEFAULT 1,
+  FOREIGN KEY (varenummer, vareversjon) REFERENCES Vare (varenummer, versjon)
 );
 
-INSERT INTO Medlem(id, navn, aktiv) VALUES (1, 'Martin', 0), (2, 'Nanna', 1), (3, 'Marius', 1);
+INSERT INTO Medlem (id, navn, aktiv) VALUES
+  (1, 'Martin', 0),
+  (2, 'Nanna', 1),
+  (3, 'Marius', 1);
 
-INSERT INTO Ordre(id, medlemId) VALUES (1, 1), (2, 1), (3, 2);
+INSERT INTO Ordre (id, medlemId, ordreDato) VALUES
+  (1, 1, TIMESTAMP '25-03-2017 17:00:00'),
+  (2, 1, TIMESTAMP '25-03-2017 18:00:00'),
+  (3, 2, TIMESTAMP '25-03-2017 19:00:00'),
+  (4, 3, TIMESTAMP '25-03-2017 17:30:00');
 
-INSERT INTO OrdreLinje(id, ordreId, antall, enhetsPris) VALUES
-  (1, 1, 2, 50), (2, 1, 3, 20), (3, 2, 1, 1000), (4, 3, 5, 150), (5, 3, 6, 200), (6, 3, 1, 500);
+INSERT INTO Vare (varenummer, versjon, varenavn, pris) VALUES
+  (1, 1, 'Genser', 299),
+  (1, 2, 'Genser', 249),
+  (2, 1, 'Bukse', 699),
+  (3, 1, 'Sokker', 49),
+  (3, 2, 'Sokker', 69),
+  (3, 3, 'Sokker', 39),
+  (4, 1, 'Lue', 39);
+
+INSERT INTO OrdreLinje (id, ordreId, varenummer, vareversjon, antall) VALUES
+  (1, 1, 3, 1, 2), -- Sokker 49 17:00
+  (2, 2, 3, 3, 1), -- Sokker 39 18:00
+  (3, 2, 1, 2, 1), -- Genser 249 18:00
+  (4, 3, 2, 1, 5), -- Bukse 699 19:00
+  (5, 3, 3, 2, 6), -- Sokker 69 19:00
+  (6, 3, 4, 1, 1), -- Lue 39 19:00
+  (7, 4, 1, 1, 5), -- Genser 299 17:30
+  (7, 4, 3, 1, 2); -- Sokker 49 17:30
